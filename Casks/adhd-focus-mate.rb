912e517d7ed47082ed6ad58c96a0de48dd0c09f7
spaceman1412 <spacemannormal@gmail.com>
2026-02-04 21:34:26 +0700

    fix: Update cask URLs and homepage to reflect repository rename from `adhd-focus-timer` to `adhd-focus-mate`.

--- Casks/adhd-focus-mate.rb
+++ Casks/adhd-focus-mate.rb
@@ -2,10 +2,10 @@ cask "adhd-focus-mate" do
   version "1.0.0"
   sha256 "dd83efff9167bcee67eba5f70316bce74e3a71788815e2c190b708ef770c7858"
 
-  url "https://github.com/skainguyen1412/adhd-focus-timer/releases/download/v#{version}/ADHD-Focus-Mate.zip"
+  url "https://github.com/skainguyen1412/adhd-focus-mate/releases/download/v#{version}/ADHD-Focus-Mate.zip"
   name "ADHD Focus Mate"
   desc "The ADHD timer that sees your distractions before you get lost in them"
-  homepage "https://github.com/skainguyen1412/adhd-focus-timer"
+  homepage "https://github.com/skainguyen1412/adhd-focus-mate"
 
   depends_on macos: ">= :sonoma"
 
